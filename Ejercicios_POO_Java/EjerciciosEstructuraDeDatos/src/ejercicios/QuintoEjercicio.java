e460cdcb4eea1a78f8f57a0418d180297ee932be
Aitor Lepe <globix28@gmail.com>
2016-02-25 15:27:46 +0100

    Primera subida proyecto 'EjerciciosEstructurasDeDatos'

---
+++ Ejercicios_POO_Java/EjerciciosEstructuraDeDatos/src/ejercicios/QuintoEjercicio.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+import main.Main;
+
+public class QuintoEjercicio {
+
+	public static void main(String[] args) {
+		System.out.println("Not implemented yet");
+		Main.main(null);
+	}
+
+}
---
+++ Ejercicios_POO_Java/EjerciciosEstructuraDeDatos/src/ejercicios/SextoEjercicio.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+import main.Main;
+
+public class SextoEjercicio {
+
+	public static void main(String[] args) {
+		System.out.println("Not implemented yet");
+		Main.main(null);
+	}
+
+}
